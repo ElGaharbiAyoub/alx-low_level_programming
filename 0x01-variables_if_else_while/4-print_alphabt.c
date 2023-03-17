f4d5bf3bdcc8de6b20ade514457cc7e11ef57a36
elgharbi <elgharbiay@gmail.com>
2023-03-17 08:24:35 -0700

    program that prints the alphabet in lowercase, exept e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i=97;
+
+	while (i <= 122)
+	{
+		if (i == 101 || i == 113)
+		{
+			i++;
+			continue;
+		}
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
