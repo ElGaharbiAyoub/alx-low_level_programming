867b9769bc5afdeeb5276479fcd5f8b188fd0c9c
elgharbi <elgharbiay@gmail.com>
2023-03-21 15:11:16 -0700

    function that prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the alphabet 10 times in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int times = 1;
+
+	while (times <= 10)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 
 char _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
