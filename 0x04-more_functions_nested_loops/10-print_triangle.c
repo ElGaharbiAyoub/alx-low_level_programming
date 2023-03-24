4435e40cd2ac28f396bdf289aa89aa1504b236b7
elgharbi <elgharbiay@gmail.com>
2023-03-23 19:19:24 -0700

    task prints a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+
+#include "main.h"
+
+/**
+ * print_triangle -  checks for checks for a digit (0 through 9).
+ * @size: size -  Variable
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= size; co++)
+		{
+			for (rs = co; rs < size; rs++)
+			{
+				_putchar(' ');
+			}
+			for (ro = 1; ro <= co; ro++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
