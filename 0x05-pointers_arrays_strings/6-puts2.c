57242df2e432a303923dc065b6395331c6277f61
elgharbi <elgharbiay@gmail.com>
2023-03-27 17:33:46 -0700

    unction that prints every other character of a string, starting with the first character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,17 @@ void _puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (i >= 0)
 	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
 		i++;
 	}
-	 _putchar('\n');
 }
