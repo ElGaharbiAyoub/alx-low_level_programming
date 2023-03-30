b474a93b5de9dd8185dd98d5e9f8d8ad0ca9b39a
elgharbi <elgharbiay@gmail.com>
2023-03-30 15:55:11 -0700

    prints an integer using putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,29 +6,27 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	int count, y, k;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		m = n * -1;
-	}
+	y = 1000000000;
+
+	if (n == 0)
+		_putchar('0');
+	else if (n > 0)
+		n *= -1;
 	else
+		_putchar('-');
+	for (count = 0; count < 10; count++, y /= 10)
 	{
-		m = n;
-	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
+		if (n / y == 0)
+			continue;
+		else
+		{
+			k = (-(n / y) % 10);
+			if (k < 0)
+				k *= -1;
+			_putchar(k + '0');
+		}
 	}
 }
+
