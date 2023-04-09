aa40b946f5a880df0e20afb607be3c2d82f15a91
elgharbi <elgharbiay@gmail.com>
2023-04-09 15:52:51 -0700

    function that prints its name followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - function that prints its name followed by a new line.
+ * @argc: count of the arguments.
+ * @argv: pointers to the strings which are those arguments.
+ * Return: 0 if successful.
+ */
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
