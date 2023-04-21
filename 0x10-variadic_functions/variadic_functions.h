3715222195d6e0c06b94edd37bc3a26db2a97396
elgharbi <elgharbiay@gmail.com>
2023-04-20 17:27:11 -0700

    hearder file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef _VARIADIC_FUNCTION_H
+#define _VARIADIC_FUNCTION_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+#endif /** _VARIADIC_FUNCTION_H*/
+
