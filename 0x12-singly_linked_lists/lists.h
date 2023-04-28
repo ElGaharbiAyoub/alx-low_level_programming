5174fad88cd020342640b40d0527553cb7f290a0
elgharbi <elgharbiay@gmail.com>
2023-04-27 17:30:34 -0700

    fixe

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -29,7 +30,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 
 
 #endif /**_0_PRINT_LIST.H*/
