258eddb2ae9c1e853de11b4077ff701600623952
elgharbi <elgharbiay@gmail.com>
2023-05-01 07:32:14 -0700

    function that print node of list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+
+/**
+ * print_listint - print nodes
+ * @h: node
+ * Return: count of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count;
+
+	for (count = 0; h != NULL; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
