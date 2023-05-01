8b9ed33658cda0cd192b188890f17da767477a84
elgharbi <elgharbiay@gmail.com>
2023-05-01 07:43:49 -0700

    fixe

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 
-
 /**
  * print_listint - print nodes
  * @h: node
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
