299ffc021727aa61df11d9dacc22fc88fdfbf17e
elgharbi <elgharbiay@gmail.com>
2023-05-01 07:19:20 -0700

    fixe

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -31,3 +31,5 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
+
+#endif
