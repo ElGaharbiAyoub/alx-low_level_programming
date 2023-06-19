b17acbe00737e0c7fe70f2a6a3b7b73fda97d07d
elgharbi <elgharbiay@gmail.com>
2023-06-19 16:54:42 -0700

    aadv

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo -e '#include <stdio.h>\n\nunsigned int sleep(unsigned int seconds)\n{\n    return 0;\n}' > sleep.c
 gcc -shared -o sleep.so -fPIC sleep.c
 export LD_PRELOAD=$PWD/sleep.so
