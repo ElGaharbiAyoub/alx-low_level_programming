c2eb778a66a4f8c581b4c8a64dec7cee4b0f6d89
elgharbi <elgharbiay@gmail.com>
2023-06-19 17:02:20 -0700

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ElGaharbiAyoub/alx-low_level_programming/raw/main/0x18-dynamic_libraries/sleep.so
+wget -P /tmp https://github.com/ElGaharbiAyoub/alx-low_level_programming/raw/master/0x18-dynamic_libraries/sleep.so
 export LD_PRELOAD=/tmp/sleep.so
