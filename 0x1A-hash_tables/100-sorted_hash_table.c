918e3126d12b75877c96f52e4da138e400cd885d
elgharbi <elgharbiay@gmail.com>
2023-07-09 23:32:52 +0100

    sorted hash table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -20,6 +20,8 @@ shash_table_t *shash_table_create(unsigned long int size)
 
 	new_table->size = size;
 	new_table->array = malloc(sizeof(shash_node_t *) * size);
+	new_table->shead = NULL;
+	new_table->stail= NULL;
 	if (!new_table->array)
 	{
 		free(new_table);
@@ -97,7 +99,52 @@ shash_node_t *add_node(shash_node_t **head, const char *key, const char *value)
 	new->next = *head;
 
 	*head = new;
+	new->snext = NULL;
+	new->sprev = NULL;
 	return (*head);
+}
+
+/**
+ * sorted_linked_list - sets doubly linked list
+ * @ht: hash table to go through
+ * @new: node to sort
+ * Return: pass or fail
+ */
+int sorted_linked_list(shash_table_t *ht, shash_node_t *new)
+{
+	shash_node_t *sorter;
+
+	if (!ht->shead)
+	{
+		ht->shead = new;
+		ht->stail = new;
+		return (1);
+	}
+	if (strcmp(new->key, ht->shead->key) < 0)
+	{
+		ht->shead->sprev = new;
+		new->snext = ht->shead;
+		ht->shead = new;
+		return (1);
+	}
+	else
+	{
+		sorter = ht->shead->snext;
+		while (sorter && strcmp(new->key, sorter->key) > 0)
+			sorter = sorter->snext;
+		if (!sorter)
+		{
+			new->sprev = ht->stail;
+			ht->stail->snext = new;
+			ht->stail = new;
+			return (1);
+		}
+		new->sprev = sorter->sprev;
+		sorter->sprev->snext = new;
+		sorter->sprev = new;
+		new->snext = sorter;
+	}
+	return (1);
 }
 
 /**
@@ -131,5 +178,109 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	}
 	else if (isUpdate == -1)/* If memory allocation for value update failed */
 		return (0);
-	return (1);
+	return (sorted_linked_list(ht, new));
+}
+
+/**
+ * shash_table_get - Retrieves the value associated with a key in a hash table.
+ * @ht: Pointer to the hash table.
+ * @key: The key to search for.
+ *
+ * Return: Pointer to the value associated with the key, or NULL if not found.
+ */
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	shash_node_t *node;
+
+	if (!ht)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	while (node)
+	{
+		/* Compare the current node's key with the provided key */
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+
+}
+
+/**
+ * shash_table_print - Prints a hash table.
+ * @ht: Pointer to the hash table.
+ */
+void shash_table_print(const shash_table_t *ht)
+{
+	unsigned long int isComa = 0;
+	shash_node_t *node;
+
+	if (ht)
+	{
+		printf("{");
+		node = ht->shead;
+		while (node)
+		{
+			if (isComa)
+				printf(", ");
+			printf("\'%s\': \'%s\'", node->key, node->value);
+			isComa = 1;
+			node = node->snext;
+		}
+		printf("}\n");
+	}
+}
+
+/**
+ * shash_table_print_rev - prints table reversed
+ * @ht: hash table to print
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	unsigned long int isComa = 0;
+	shash_node_t *tail;
+
+	if (ht)
+	{
+		printf("{");
+		tail = ht->stail;
+		while (tail)
+		{
+			if (isComa)
+				printf(", ");
+			printf("\'%s\': \'%s\'", tail->key, tail->value);
+			isComa = 1;
+			tail = tail->sprev;
+		}
+		printf("}\n");
+	}
+}/**
+ * shash_table_delete - Deletes a hash table.
+ * @ht: Pointer to the hash table.
+ */
+void shash_table_delete(shash_table_t *ht)
+{
+	unsigned long int i;
+	shash_node_t *tmp, *delete;
+
+	if (ht)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			tmp = ht->array[i];
+			while (tmp)
+			{
+				delete = tmp;
+				tmp = tmp->next;
+				free(delete->key);
+				free(delete->value);
+				free(delete);
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
 }
