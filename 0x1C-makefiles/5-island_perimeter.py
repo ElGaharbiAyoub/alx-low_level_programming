eb9b6b97870236eef1a0219e4f2a9115f228dcb5
elgharbi <elgharbiay@gmail.com>
2023-08-24 02:04:32 +0100

    perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""fff""""
+
+def island_perimeter(grid):
+    """ sss"""
+    count = 0
+    if grid:
+        gridLen = len(grid)
+        rowLen = len(grid[0])
+        for index1, row in enumerate(grid):
+            for index2, zone in enumerate(row):
+                if zone:
+                    if index1 == 0:
+                        count += 1
+                    else:
+                        if not grid[index1 - 1][index2]:
+                            count += 1
+                    if index1 >= gridLen - 1:
+                        count += 1
+                    else:
+                        if not grid[index1 + 1][index2]:
+                            count += 1
+                    if index2 == 0:
+                        count += 1
+                    else:
+                        if not row[index2 - 1]:
+                            count += 1
+                    if index2 >= rowLen - 1:
+                        count += 1
+                    else:
+                        if not row[index2 + 1]:
+                            count += 1
+    return count
+    
